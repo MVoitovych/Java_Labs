030c62e4b7e1aef40e429f5b365728e1622f8623
Mykhailo Voitovych <m.voitovych21@gmail.com>
2022-03-31 20:59:48 +0300

    lab 2 code

---
+++ src/ua/lviv/iot/lab2/manager/FishingTackleManager.java
@@ -0,0 +1,22 @@
+package ua.lviv.iot.lab2.manager;
+
+import ua.lviv.iot.lab2.models.FishingTackle;
+import ua.lviv.iot.lab2.models.TypeOfFishing;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class FishingTackleManager {
+    private ArrayList<FishingTackle> tackles;
+
+    public ArrayList<Map<String, Integer>> tackleSortByPrice(){
+        return null;
+    }
+    public ArrayList<Map<String, String>> tackleSortByAlphabet(){
+        return null;
+    }
+    public ArrayList<Map<String, String>> tackleSortByTypeOfFishing(TypeOfFishing neededType){
+        return null;
+    }
+
+}
